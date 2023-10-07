084c5e1161586134902e673493484d457a80cc59
IVBunin <120949130+IVBunin@users.noreply.github.com>
2023-10-08 02:02:44 +1000

    modified:   config.py 	modified:   main.py 	new file:   templates.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 from telebot.apihelper import ApiTelegramException
 
 #@bot.message_handler(content_types = ['comand name']) 
-
+ 
 
 
 bot.infinity_polling(none_stop=True, timeout=10)
---
+++ templates.py
@@ -0,0 +1,6 @@
+try:#обработка ошибок 
+        bot.send_message(message.chat.id)
+except ApiTelegramException as e:
+    if e.description == "Forbidden: bot was blocked by the user":
+        return 1 
+        
