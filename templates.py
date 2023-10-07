3874ecce6b88461f5946313fe2187d3506870b6c
IVBunin <120949130+IVBunin@users.noreply.github.com>
2023-10-08 02:15:26 +1000

    new file:   1.xlsx 	new file:   pip installs.txt 	new file:   readexell.py 	modified:   templates.py

--- templates.py
+++ templates.py
@@ -2,5 +2,5 @@ try:#обработка ошибок
         bot.send_message(message.chat.id)
 except ApiTelegramException as e:
     if e.description == "Forbidden: bot was blocked by the user":
-        return 1 
+        return 1  
         
