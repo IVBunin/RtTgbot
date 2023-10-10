0066df72a64151798ca3341b4f2130a306626dc3
IVBunin <120949130+IVBunin@users.noreply.github.com>
2023-10-10 16:57:26 +1000

    modified:   config.py 	modified:   main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import telebot
+import config as cfg
 from telebot.apihelper import ApiTelegramException
 
 #@bot.message_handler(content_types = ['comand name']) 
- 
+
+bot = telebot.TeleBot(cfg._TOKEN_)
+
 
 
 bot.infinity_polling(none_stop=True, timeout=10)
