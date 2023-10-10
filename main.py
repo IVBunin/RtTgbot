eedde6bd3742739ecfe9be1415d19763e99ecef3
IVBunin <120949130+IVBunin@users.noreply.github.com>
2023-10-10 17:08:20 +1000

    modified:   main.py 	modified:   pip installs.txt

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from telebot.apihelper import ApiTelegramException
 
 bot = telebot.TeleBot(cfg._TOKEN_)
 
-
-
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    try:
+        bot.send_message(message.chat.id,text = "Привет {0.first_name}. я бот \n/butons - выводит кнопки \n Так же он принимает картинки 😉😉😉".format(message.from_user))
+        
+    except ApiTelegramException as e:
+        pass 
 bot.infinity_polling(none_stop=True, timeout=10)
