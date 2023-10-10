e66d6c85aeecde7cce9f4fc5153473758b0a9637
IVBunin <120949130+IVBunin@users.noreply.github.com>
2023-10-10 21:55:37 +1000

    modified:   __pycache__/prompts.cpython-311.pyc 	modified:   pip installs.txt 	modified:   prompts.py Ы	modified:   templates.py

--- templates.py
+++ templates.py
@@ -1,6 +1,29 @@
-try:#обработка ошибок 
-        bot.send_message(message.chat.id)
-except ApiTelegramException as e:
-    if e.description == "Forbidden: bot was blocked by the user":
-        return 1  
-        
+import telebot
+import time
+import aiogram
+
+
+@tb.message_handler(func=lambda m: True)
+def echo_all(message):
+    try:
+           time.sleep(20) # to make delay 
+           ret_msg=tb.reply_to(message, "response message") 
+           print(ret_msg)
+           assert ret_msg.content_type == 'text'
+    except TelegramResponseException  as e:
+            print(e) # do not handle error #403
+    except Exception as e:
+            print(e) # do not handle error #403
+    except AssertionError:
+            print( "!!!!!!! user has been blocked !!!!!!!" ) # do not handle error #403
+     
+
+tb.polling(none_stop=True, timeout=123)
+    
+bot.send_message(message.chat.id, '__Нижнее подчёркивание__', parse_mode='MarkdownV2')
+bot.send_message(message.chat.id, '~Зачёркнутый~', parse_mode='MarkdownV2')
+
+rom aiogram.utils.markdown import hlink
+
+hello_with_url = hlink('Привіт', 'https://pypi.org/project/pyTelegramBotAPI/#description')
+bot.send_message(message.chat.id ,f'{hello_with_url}, я чат-бот ', reply_markup=markup,parse_mode='HTML')
