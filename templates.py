4a9d223a1b9797f0186351fcf80acbccc9f975b8
ExcellRover <120949130+IVBunin@users.noreply.github.com>
2023-10-11 15:38:22 +1000

    Update templates.py

--- templates.py
+++ templates.py
@@ -3,6 +3,15 @@ import time
 import aiogram
 
 
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    msg = bot.send_message(message.chat.id, 'Введите то то')
+    bot.register_next_step_handler(msg, start_2)
+
+
+def start_2(message):
+    bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id - 1, text='вы ввели ' + message.text)
 @tb.message_handler(func=lambda m: True)
 def echo_all(message):
     try:
