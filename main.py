5919a006b13325af915474ababb08dc80d39776c
XeITiZFeeD <hoilalex47@gmail.com>
2023-10-12 02:31:01 +1000

    Добавил ассинхронность, логгирование + вывод информации из таблички

--- main.py
+++ main.py
@@ -1,45 +1,76 @@
 import telebot
+import aiohttp
+import asyncio
+import xlrd
+import random
 import config as cfg
 import prompts as prompt
 from telebot import types
 from telebot.apihelper import ApiTelegramException
+from telebot.async_telebot import AsyncTeleBot
 
 #@bot.message_handler(content_types = ['comand name']) 
 
-bot = telebot.TeleBot(cfg._TOKEN_)
+bot = AsyncTeleBot(cfg._TOKEN_)
+
+async def writelog(txt):
+    file = open('log.txt', 'a')
+    file.write(txt +"\n")
+    file.close
+    print(txt)
 
 @bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
+async def send_welcome(message):
+
+
     try:
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn0 = types.KeyboardButton('Статус заявки')
         btn1 = types.KeyboardButton('Возможность до адреса')
-        markup.add(btn0,btn1)
+        btn2 = types.KeyboardButton('Тестовый вывод')
+        markup.add(btn0, btn1, btn2)
         bot.send_message(message.chat.id,text = "Добрый день {0.first_name}, это - телеграм бот компании Ростелеком, в нем вы можете проверить возможность до адреса и статус вашей заявки".format(message.from_user),reply_markup=markup)
     except ApiTelegramException as e:
         pass 
 
+
 @bot.message_handler(commands = ['address'])
-def adres_possibility(message):
+async def adres_possibility(message):
     try: 
         bot.send_message(message.chat.id,prompt.text_enter_address)
     except ApiTelegramException as e:
         pass 
 
 @bot.message_handler(commands=['application']) 
-def task_chek(message):
+async def task_chek(message):
     try: 
         bot.send_message(message.chat.id,prompt.text_enter_application)
     except ApiTelegramException as e:
         pass 
-    
+
+
+
+
 @bot.message_handler(func=lambda message: True)
-def reg(message):
+async def reg(message):
+    id = message.chat.id
+    txt =('('+ str(id)+')' + message.chat.username + ' : ' +str(message.text))
+    await writelog(txt) 
+    
+    spreadsheet = xlrd.open_workbook('1.xls', formatting_info=True)
+    sheet = spreadsheet.sheet_by_index(0)
+    integ = random.randint(0,sheet.nrows-3)
+    row = sheet.cell_value(rowx=integ, colx=1)
+    print(row, integ)
+    if message.text == 'Тестовый вывод':
+        await bot.send_message(id, row)
     if message.text == 'Статус заявки':
-        bot.send_message(message.chat.id,prompt.text_enter_application)
+        await bot.send_message(id,prompt.text_enter_application)
     if message.text == 'Возможность до адреса':
-        bot.send_message(message.chat.id,prompt.text_enter_address)
+        await bot.send_message(id,prompt.text_enter_address)
 
 
 
-bot.infinity_polling(none_stop=True, timeout=10)
+
+
+asyncio.run(bot.polling())
