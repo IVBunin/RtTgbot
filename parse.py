6516a60f67e4983cab5302a6ad34bea2f5f096d3
IVBunin <120949130+IVBunin@users.noreply.github.com>
2023-10-30 14:06:46 +1000

    modified:   1.xls 	modified:   address.py 	modified:   application.py 	modified:   log.txt 	deleted:    parse.py 	deleted:    readexell.py

--- parse.py
+++
@@ -1,9 +0,0 @@
-from requests import get 
-from bs4 import BeautifulSoup as bs
-import pandas as pd
- 
-
-aplication  =  get("https://192.168.0.1")
-
-print(aplication.status_code)
-
--- readexell.py
+++
@@ -1,7 +0,0 @@
-#Файл реализации логики работы с бд в экселе
-import pandas as pd
- 
-
-dataframe1 = pd.read_excel('1.xlsx')
- 
-print(dataframe1)
