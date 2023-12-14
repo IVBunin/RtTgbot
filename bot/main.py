1951abcfbae28de0153426a169495bc28dfc1819
IVBunin <120949130+IVBunin@users.noreply.github.com>
2023-12-14 23:37:14 +1000

    modified:   bot/main.py 	deleted:    class.py

--- bot/main.py
+++ bot/main.py
@@ -20,39 +20,41 @@ async def send_welcome(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn0 = types.KeyboardButton('тарифы, акции и услуги')
         btn1 = types.KeyboardButton('Информационные услуги')
-        btn2 = types.KeyboardButton('Информационные услуги 2')
-        btn3 = types.KeyboardButton('?')
-        btn4 = types.KeyboardButton('<=')
-        btn5 = types.KeyboardButton('✉️')
-        markup.add(btn0, btn1, btn2, btn3, btn4, btn5)
-        await bot.send_message(message.chat.id,text = "Добрый день {0.first_name}, это - телеграм бот компании Ростелеком, в нем вы можете проверить возможность до адреса и статус вашей заявки".format(message.from_user),reply_markup=markup)
+        markup.add(btn0, btn1)
+        await bot.send_message(message.chat.id,text = "Добрый день {0.first_name}, это - телеграм бот компании Ростелеком, в нем вы можете проверить возможность до адреса и узнать о тарифах".format(message.from_user),reply_markup=markup)
     except ApiTelegramException as e:
-        raise(Exception)
+        print(e)
 
 #Выбор функциональных групп 
 @bot.message_handler(func=lambda message: True)
 async def reg(message):
     try:
+
         if message.text == "тарифы, акции и услуги":
-            await bot.send_message(message.chat.id, text = "a1")
+            await bot.send_message(message.chat.id, text = "Введите тариф")
 
-        if message.text == "Информационные услуги":
-            await bot.send_message(message.chat.id, text = " a2")
-
-        if message.text == "Информационные услуги 2":
-            await bot.send_message(message.chat.id, text = " 3")
-
-        if message.text == "<=":
-            await bot.send_message(message.chat.id, text = "a4 ")
-
-        if message.text == "?":
-            await bot.send_message(message.chat.id, text = " a5")
-
-        if message.text == "✉️":
-            await bot.send_message(message.chat.id, text = " a6")
-
+        if message.text == "Возможность до адреса":
+            await bot.send_message(message.chat.id, text = "Введите адрес")
+            
     except ApiTelegramException as e:
-        raise(Exception)
-        
+        print(e)
+
+@bot.message_handler(func=lambda message: True)
+async def serch_in_db_adress(message):
+    try:    
+        options_for_address = serch_in_db(message.text,address)
+        await bot.send_message(message.chat.id, text = f"Тарифы для вашего адреса = {options_for_address}")
+    except ApiTelegramException as e:
+        print(e)
+
+@bot.message_handler(func=lambda message: True)
+async def serch_in_db_adress(message):
+    try:    
+        options= serch_in_db(message.text,option)
+        await bot.send_message(message.chat.id, text = f"Тарифы = {options}")
+    except ApiTelegramException as e:
+        print(e)
+
+
 if __name__ == "__main__":
     asyncio.run(bot.polling())
