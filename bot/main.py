aea06c4a84dd492e8a27d4cf5bdcffe56891bf33
IVBunin <120949130+IVBunin@users.noreply.github.com>
2024-01-21 21:12:03 +1000

    modified:   bot/__pycache__/config.cpython-311.pyc 	modified:   bot/__pycache__/main_logic.cpython-311.pyc 	new file:   bot/__pycache__/registration.cpython-311.pyc 	modified:   bot/main.py

--- bot/main.py
+++ bot/main.py
@@ -5,11 +5,15 @@ import config as cfg
 from telebot import types
 from telebot.apihelper import ApiTelegramException
 from telebot.async_telebot import AsyncTeleBot
-from main_logic import * #Тут можно 
+from datetime import datetime
+#самописные библиотеки импортируем польностью 
+from registration import * 
+from main_logic import *
+from time import sleep
 #Желательно весь ввод проводить через перекодирование в utf-8 иначе при вводе незнакомых символов будет краш
 #Или обработать исключение
 
-
+                                                                                      
 bot = AsyncTeleBot(cfg._TOKEN_)
 
 
@@ -20,40 +24,39 @@ async def send_welcome(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn0 = types.KeyboardButton('тарифы, акции и услуги')
         btn1 = types.KeyboardButton('Возможность до адреса')
-        markup.add(btn0, btn1)
+        btn2 = types.KeyboardButton('Регистрация')
+        markup.add(btn0, btn1,btn2)
         await bot.send_message(message.chat.id,text = "Добрый день {0.first_name}, это - телеграм бот компании Ростелеком, в нем вы можете проверить возможность до адреса и узнать о тарифах".format(message.from_user),reply_markup=markup)
     except ApiTelegramException as e:
         print(e)
+        
 
 #Выбор функциональных групп 
 @bot.message_handler(func=lambda message: True)
 async def reg(message):
     try:
-        if message.text == "тарифы, акции и услуги":
-            await bot.send_message(message.chat.id, text = "Введите тариф")
-
-        if message.text == "Возможность до адреса":
-            await bot.send_message(message.chat.id, text = "Введите адрес в формате \"Уссурийск г.,Выгонная,16,121\" ")
+        match (message.text):
+            case ("тарифы, акции и услуги"):
+                await bot.send_message(message.chat.id, text = "Введите тариф")
+            case ("Возможность до адреса"):
+                await bot.send_message(message.chat.id, text = "Введите адрес")
+            case ("Регистрация"):
+                register_user("new_base",message.from_user.first_name, str(message.chat.id))
+                await bot.send_message(message.chat.id, text = "Вы зарегистрированны")
     except ApiTelegramException as e:
         print(e)
 
 
-@bot.message_handler(func=lambda message: True)
-async def serch_in_db_adress(message):
-    try:
-# 0 - параметр для вывода адреса
-        await bot.send_message(message.chat.id, text = f"Тарифы для вашего адреса = {serch_in_db(message.text,0)}")
-    except ApiTelegramException as e:
-        print(e)
 
-@bot.message_handler(func=lambda message: True)
-async def serch_in_db_adress(message):
-    try:
-# 1 - параметр для вывода тарифов
-        await bot.send_message(message.chat.id, text = f"Тарифы = {serch_in_db( 'void', 1)}")
-    except ApiTelegramException as e:
-        print(e)
+
 
 
 if __name__ == "__main__":
+    char = [':','.','+'' ']
+    for i in range(len(char)):
+        basename = str(datetime.today())
+        basename= basename.replace(char[i],'_')
+    print(reg_init(basename))
     asyncio.run(bot.polling())
+
+    
