5830e5f3f71fb7c8728256d05a04be0b8ac6a269
IVBunin <excellrover@gmail.com>
2025-03-05 11:05:21 +1000

    modified:   bot/config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,7 @@
 # В конфиг добавляем системные переменные по типу пути и токена 
 # Может придумаете что-то ещё 
 
-_TOKEN_ = "6542632228:AAF0NMyt5LSaKTR7sI4zrY2GVj7Qtg0ceOQ"
-_GPTTOKEN_ = "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"
+_TOKEN_ = 
 master_password = "qwerty123"
 _LOCAL_BASE_PATH_ = "bot/data/base/"
 _LOCAL_PATH_ = "bot/data/"
